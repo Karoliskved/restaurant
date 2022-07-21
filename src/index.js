aca871cd19c83a6a3bc9057616ee59e7a974b043
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-21 18:23:00 +0300

    generate html elements using index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 console.log("test")
+import ihopImg from './ihop.jpeg'
+
+function component(){
+    const element = document.createElement('div')
+    const header = document.createElement('div')
+    header.classList.add('header')
+    const main=document.createElement('div')
+    main.classList.add('main')
+    const footer = document.createElement('div')
+    footer.classList.add('footer')
+    footer.textContent="contact +84 666 1337"
+    const name=document.createElement('div')
+    name.classList.add('name')
+    name.textContent="Goth Ihop"
+    const tabs=document.createElement('div')
+    tabs.classList.add('tabs')
+    const mainTab=document.createElement('div')
+    mainTab.classList.add('item')
+    mainTab.textContent="main"
+    const menuTab=document.createElement('div')
+    menuTab.classList.add('item')
+    menuTab.textContent="menu"
+    const contactTab=document.createElement('div')
+    contactTab.classList.add('item')
+    contactTab.textContent="contact"
+    const text=document.createElement('div')
+    text.classList.add('text')
+    text.textContent="Goth Ihop is the best place to get pancakes AND goths!!!!"
+    const myIcon=new Image();
+    myIcon.src=ihopImg;
+    header.appendChild(name)
+    tabs.appendChild(mainTab)
+    tabs.appendChild(menuTab)
+    tabs.appendChild(contactTab)
+    header.appendChild(tabs)
+    main.appendChild(text)
+    main.appendChild(myIcon)
+    element.appendChild(header)
+    element.appendChild(main)
+    element.appendChild(footer)
+    return element;
+}
+const container=document.querySelector("#content");
+container.appendChild(component());
