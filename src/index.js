b54c13f85af781590e56cf207ea04f7b3129cdc3
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-21 19:11:47 +0300

    add event listeners to header items

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 console.log("test")
 import createMain from './modules/createMain.js'
 
-
 document.querySelector("#content").appendChild(createMain());
+document.querySelector("#mainTab").addEventListener('click', ()=> {console.log("main")})
+document.querySelector("#menuTab").addEventListener('click', ()=> {console.log("menu")})
+document.querySelector("#contactTab").addEventListener('click', ()=> {console.log("contact")})
